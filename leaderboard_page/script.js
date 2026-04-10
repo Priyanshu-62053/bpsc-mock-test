db221138dd61c39c2a4e152cf22e17b2f02c98ae
Priyanshu-62053 <145546845+Priyanshu-62053@users.noreply.github.com>
2026-04-10 23:21:40 +0530

    Add files via upload

---
+++ leaderboard_page/script.js
@@ -0,0 +1,11 @@
+//connects both buttons :
+
+let backTotest= document.querySelector("#backTotest");
+backTotest.addEventListener("click", function(){
+  // window.location.href="/result_page/result.html";
+  window.location.href="/quistion_page/test.html";
+});
+let ViewResult= document.querySelector("#ViewResult");
+ViewResult.addEventListener("click", function(){
+  window.location.href="/result_page/result.html";
+})
